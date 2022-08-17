43be5e86681494ed1834e93731915d7517d7dc40
viniciusokamoto <leo_kamoto@hotmail.com>
2022-08-17 18:45:22 -0500

    feat(Sidebar): create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <>
     <Header/>
-    <h1>Hello Ignite Community</h1>    
+    <div className={styles.wrapper}>
+      <Sidebar/>
+      <main>
+        <h1>Feed</h1>
+      </main>
+      </div>  
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
+
+export function Sidebar() {
+
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover}
+                src='https://images.unsplash.com/photo-1608306448197-e83633f1261c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60' />
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/53801461?v=4" alt="Profile Image" />
+                <strong>Vinicius Okamoto</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+
+                <a href='#'>
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
