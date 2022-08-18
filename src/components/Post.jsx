da63a97da716e2e54c754c946244e112b342c3f4
viniciusokamoto <leo_kamoto@hotmail.com>
2022-08-17 21:35:44 -0500

    feat(Post): add style to base component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ export function Post() {
 
     return (
         <article className={styles.post}>
-            <header>
+            <header className={styles.postHeader}>
                 <div className={styles.author}>
                     <img src="https://avatars.githubusercontent.com/u/53801461?v=4" />
                     <div className={styles.authorInfo}>
@@ -17,8 +17,11 @@ export function Post() {
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href="#">jane.design/doctorcare</a></p>
-                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '}
+                    <a href="#">#rocketseat</a></p>
             </div>
         </article>
     );
