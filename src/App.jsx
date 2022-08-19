3967a4f835757c7dd93d9dcb5aea7aec1f6e118b
viniciusokamoto <leo_kamoto@hotmail.com>
2022-08-19 18:29:49 -0500

    feat(Post): add jsx code to render post and content

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,60 @@ import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post/Post.jsx';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: ' 👉 jane.design/doctorcare' },
+      { type: 'markdown', content: '#novoprojeto' },
+      { type: 'markdown', content: '#nlw' },
+      { type: 'markdown', content: '#rocketseat' },
+    ],
+    publishedAt: new Date('2022-08-19 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/viniokamoto.png',
+      name: 'Vinicius Okamoto',
+      role: 'Software Engineer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: ' 👉 jane.design/doctorcare', url: '#' },
+      { type: 'markdown', content: '#novoprojeto' },
+      { type: 'markdown', content: '#nlw' },
+      { type: 'markdown', content: '#rocketseat' },
+    ],
+    publishedAt: new Date('2022-08-17 20:00:00'),
+  },
+]
 export function App() {
+
+  const renderedPosts = posts.map(post => <Post 
+    key={post.id}
+    author={post.author} 
+    content={post.content} 
+    publishedAt={post.publishedAt} 
+  />);
 
   return (
     <>
-    <Header/>
-    <div className={styles.wrapper}>
-      <Sidebar/>
-      <main>
-        <Post/>
-        <Post/>
-      </main>
-      </div>  
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {renderedPosts}
+        </main>
+      </div>
     </>
   )
 }
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,30 +1,50 @@
+
+import {format, formatDistanceToNow} from 'date-fns';
+import enCA from 'date-fns/locale/en-CA';
 import { Avatar } from '../shared/Avatar.jsx';
 import { Comment } from './Comment/Comment.jsx';
 import { CommentForm } from './Comment/CommentForm.jsx';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({key,author, content, publishedAt}) {
+   const publishedDateFormatted = format(publishedAt, "do LLLL 'at' h:mmaaaa", {
+    locale: enCA,
+   })
 
+   const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
+    locale:enCA,
+    addSuffix: true,
+   })
+
+   var markDowns = [];
+   const contentRendered = content.map(line => {
+    if(line.type == 'paragraph'){
+        return <p>{line.content}</p>
+    }
+    if(line.type == 'link'){
+        return <p><a href={line.url}>{line.content}</a></p>
+    }
+    if(line.type ==='markdown'){
+        markDowns.push(<a href='#'>{line.content}</a>);
+    }
+   })
+
+   
     return (
         <article className={styles.post}>
             <header className={styles.postHeader}>
                 <div className={styles.author}>
-                    <Avatar src="https://avatars.githubusercontent.com/u/53801461?v=4" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Vinicius Okamoto</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title="May 14th at 2pm " dateTime="2022-05-11 08:13:30">Published 1h ago </time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeNow} </time>
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a></p>
+               {contentRendered}
+               <p>{markDowns.map(markDown => markDown)}</p>
             </div>
             <CommentForm/>
             <div className={styles.commentList}>
