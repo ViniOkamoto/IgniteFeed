952ccd3567e4075bc1c86347dbe81ebd376a3f04
viniciusokamoto <leo_kamoto@hotmail.com>
2022-08-21 17:45:43 -0500

    feat(avatar): implement props spread operator

--- ignite-feed-ts/src/components/shared/Avatar.tsx
+++ ignite-feed-ts/src/components/shared/Avatar.tsx
@@ -4,9 +4,9 @@ import styles from './Avatar.module.css';
 interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean;
 }
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true,...props }: AvatarProps) {
     return (
 
-        <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} alt={alt} />
+        <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} {...props} />
     );
 }
